7c0f6abbc5b68819e9157bded04c0bb1e159cd52
wattnotions <shaneormonde@gmail.com>
2016-01-09 16:49:45 +0000

    graps light data and date data and plots-working

--- mysql_graph.php
+++ mysql_graph.php
@@ -10,6 +10,7 @@ require_once ('../tools/jpgraph/src/jpgraph_date.php');
 $x_axis = array();
 $y_axis = array();
 $i = 0;
+
  
 $con=mysqli_connect("66.147.244.233","wattnoti_user","physics","wattnoti_plantbot");
  //Check connection
@@ -17,18 +18,27 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
  
-$result = mysqli_query($con,"SELECT * FROM sens_vals");
+$result = mysqli_query($con,"(SELECT * FROM sens_vals ORDER BY id DESC LIMIT 25) ORDER BY id ASC");
 
  
  
 while($row = mysqli_fetch_array($result)) {
-	$x_axis[$i] =  $row["date"];
+	$x_axis[$i] = strtotime($row["date"]);
+	//echo gettype($x_axis[$i]),"<br />";
 	$y_axis[$i] =  $row["Light"];
 	//echo strtotime($x_axis[$i]),"<br />";
 	$i++;
 	
  
 }
+
+
+/*
+$length = count($x_axis);
+for ($i = 0; $i < $length; $i++) {
+	echo gettype($x_axis[$i]),"<br />";
+}
+*/
 
 
      
@@ -40,9 +50,10 @@ mysqli_close($con);
 $graph = new Graph(800,500);
 $graph->img->SetMargin(40,40,40,40);	
 $graph->img->SetAntiAliasing();
-$graph->SetScale("textlin");
+$graph->SetScale("datlin");
 $graph->SetShadow();
 $graph->title->Set("Example of line centered plot");
+$graph->xaxis->SetLabelAngle(45);
 $graph->title->SetFont(FF_FONT1,FS_BOLD);
 
 
@@ -51,7 +62,7 @@ $graph->title->SetFont(FF_FONT1,FS_BOLD);
 $graph->yscale->SetGrace(0);
 
 
-$p1 = new LinePlot($y_axis);
+$p1 = new LinePlot($y_axis,$x_axis);
 $p1->mark->SetType(MARK_FILLEDCIRCLE);
 $p1->mark->SetFillColor("red");
 $p1->mark->SetWidth(4);
